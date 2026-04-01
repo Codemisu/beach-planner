845116b48ea9ca561015cf591a65d0bc3580f186
María Báez <baezmarreromaria9@gmail.com>
2026-04-01 19:16:46 +0100

    Wire weather main class with controller and dependencies

--- weather-module/src/main/java/es/ulpgc/beachplanner/weather/Main.java
+++ weather-module/src/main/java/es/ulpgc/beachplanner/weather/Main.java
@@ -1,7 +1,16 @@
 package es.ulpgc.beachplanner.weather;
 
+import es.ulpgc.beachplanner.weather.controller.WeatherController;
+import es.ulpgc.beachplanner.weather.feeder.OpenMeteoFeeder;
+import es.ulpgc.beachplanner.weather.repository.SQLiteWeatherRepository;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Weather module running");
+        WeatherController controller = new WeatherController(
+                new OpenMeteoFeeder(),
+                new SQLiteWeatherRepository()
+        );
+
+        controller.run();
     }
 }
