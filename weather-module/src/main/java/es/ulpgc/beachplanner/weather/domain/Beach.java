580950738fdeef497ea55554eaa27ffefd652c8f
María Báez <baezmarreromaria9@gmail.com>
2026-04-01 19:04:01 +0100

    Add initial weather models

--- weather-module/src/main/java/es/ulpgc/beachplanner/weather/domain/Beach.java
+++ weather-module/src/main/java/es/ulpgc/beachplanner/weather/domain/Beach.java
@@ -1,2 +1,4 @@
 package es.ulpgc.beachplanner.weather.domain;
 
+public record Beach(String name, double latitude, double longitude) {
+}
--- weather-module/src/main/java/es/ulpgc/beachplanner/weather/domain/WeatherRecord.java
+++ weather-module/src/main/java/es/ulpgc/beachplanner/weather/domain/WeatherRecord.java
@@ -1,1 +1,52 @@
 package es.ulpgc.beachplanner.weather.domain;
+
+public class WeatherRecord {
+    private String beachName;
+    private String forecastTime;
+    private Double airTemperature;
+    private Double windSpeed;
+    private String capturedAt;
+
+    public WeatherRecord() {
+    }
+
+    public String getBeachName() {
+        return beachName;
+    }
+
+    public void setBeachName(String beachName) {
+        this.beachName = beachName;
+    }
+
+    public String getForecastTime() {
+        return forecastTime;
+    }
+
+    public void setForecastTime(String forecastTime) {
+        this.forecastTime = forecastTime;
+    }
+
+    public Double getAirTemperature() {
+        return airTemperature;
+    }
+
+    public void setAirTemperature(Double airTemperature) {
+        this.airTemperature = airTemperature;
+    }
+
+    public Double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(Double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getCapturedAt() {
+        return capturedAt;
+    }
+
+    public void setCapturedAt(String capturedAt) {
+        this.capturedAt = capturedAt;
+    }
+}
