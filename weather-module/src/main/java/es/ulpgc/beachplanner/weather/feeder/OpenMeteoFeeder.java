b471766f6ee2a863de0e39f70e389e707ba9476e
María Báez <baezmarreromaria9@gmail.com>
2026-04-01 19:07:40 +0100

    Add initial weather controller feeder and repo implementations

--- weather-module/src/main/java/es/ulpgc/beachplanner/weather/feeder/OpenMeteoFeeder.java
+++ weather-module/src/main/java/es/ulpgc/beachplanner/weather/feeder/OpenMeteoFeeder.java
@@ -1,1 +1,12 @@
 package es.ulpgc.beachplanner.weather.feeder;
+
+import es.ulpgc.beachplanner.weather.domain.WeatherRecord;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OpenMeteoFeeder implements WeatherFeeder {
+    @Override
+    public List<WeatherRecord> fetch() {
+        return new ArrayList<>();
+    }
+}
