43eaf1d093bc6a582ebecf9b0effb8649a72609a
María Báez <baezmarreromaria9@gmail.com>
2026-04-01 18:56:03 +0100

    Add initial module entry point

--- weather-module/src/main/java/es/ulpgc/beachplanner/weather/feeder/WeatherFeeder.java
+++ weather-module/src/main/java/es/ulpgc/beachplanner/weather/feeder/WeatherFeeder.java
@@ -1,1 +1,7 @@
-package es.ulpgc.beachplanner.weather.feeder;
+package es.ulpgc.beachplanner.weather;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Weather module started");
+    }
+}
