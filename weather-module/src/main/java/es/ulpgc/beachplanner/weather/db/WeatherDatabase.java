035ba8096e69dd3d85f26cd81d82d626e81b871e
María Báez <baezmarreromaria9@gmail.com>
2026-04-14 11:58:47 +0100

    weather.database creo archivo y tabla en sqlite

--- weather-module/src/main/java/es/ulpgc/beachplanner/weather/db/WeatherDatabase.java
+++ weather-module/src/main/java/es/ulpgc/beachplanner/weather/db/WeatherDatabase.java
@@ -1,4 +1,35 @@
 package es.ulpgc.beachplanner.weather.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class WeatherDatabase {
+
+    private static final String URL = "jdbc:sqlite:weather.db"; ///creo weather.db
+
+    public Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+
+    public void initialize() {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS weather_records (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    beach_name TEXT NOT NULL,
+                    forecast_time TEXT NOT NULL,
+                    temperature REAL,
+                    wind_speed REAL,
+                    captured_at TEXT NOT NULL
+                );
+                """;
+
+        try (Connection connection = connect();
+             Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error initializing database", e);
+        }
+    }
 }
